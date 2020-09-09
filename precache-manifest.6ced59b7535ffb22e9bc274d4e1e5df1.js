e01c590a939759a80ad0b1d900f929bfd991d0b3
Your Name <kuberg8@gmail.com>
2020-09-09 14:46:47 +0500

    Updates

---
+++ precache-manifest.6ced59b7535ffb22e9bc274d4e1e5df1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "136819d6921aa5e8e95abaa42f7c5994",
+    "url": "/test2/index.html"
+  },
+  {
+    "revision": "4fbd6de1a7e37ece9dbf",
+    "url": "/test2/static/css/main.ebaec4f2.chunk.css"
+  },
+  {
+    "revision": "6e3edd610816920e6e09",
+    "url": "/test2/static/js/2.87338a27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test2/static/js/2.87338a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fbd6de1a7e37ece9dbf",
+    "url": "/test2/static/js/main.b5f37f89.chunk.js"
+  },
+  {
+    "revision": "20f733da752f7b89464c",
+    "url": "/test2/static/js/runtime-main.9d53dc08.js"
+  },
+  {
+    "revision": "524af6ecc6fd1dd48485cc26f25ae8b6",
+    "url": "/test2/static/media/eye-off.524af6ec.svg"
+  },
+  {
+    "revision": "150b968e68c60089422a904d7259f053",
+    "url": "/test2/static/media/eye.150b968e.svg"
+  },
+  {
+    "revision": "eb5dc4c73b414081894b4bfadae07259",
+    "url": "/test2/static/media/grid.eb5dc4c7.svg"
+  },
+  {
+    "revision": "b1193903a396e8b58e7b942e7ac731e4",
+    "url": "/test2/static/media/heart.b1193903.svg"
+  },
+  {
+    "revision": "26fb9fab31a4f642fe381676540e719e",
+    "url": "/test2/static/media/list.26fb9fab.svg"
+  }
+]);
